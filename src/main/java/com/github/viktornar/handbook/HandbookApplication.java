46612c86271aed4736009732c78557986c890ece
Viktor Nareiko <vnareiko.lt@gmail.com>
2021-08-18 23:38:36 +0300

    feat(2): Added jdbi support in spring based application.

--- src/main/java/com/github/viktornar/handbook/HandbookApplication.java
+++ src/main/java/com/github/viktornar/handbook/HandbookApplication.java
@@ -3,9 +3,11 @@ package com.github.viktornar.handbook;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
 @EnableConfigurationProperties(HandbookProperties.class)
+@EnableTransactionManagement
 public class HandbookApplication {
     public static void main(String[] args) {
         SpringApplication.run(HandbookApplication.class, args);
--- src/main/java/com/github/viktornar/handbook/HandbookConfig.java
+++ src/main/java/com/github/viktornar/handbook/HandbookConfig.java
@@ -1,13 +1,23 @@
 package com.github.viktornar.handbook;
 
+import lombok.extern.slf4j.Slf4j;
+import org.jdbi.v3.core.Jdbi;
+import org.jdbi.v3.core.h2.H2DatabasePlugin;
+import org.jdbi.v3.core.mapper.RowMapper;
+import org.jdbi.v3.core.spi.JdbiPlugin;
+import org.jdbi.v3.sqlobject.SqlObjectPlugin;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.TransactionAwareDataSourceProxy;
 
+import javax.sql.DataSource;
+import java.util.List;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ThreadFactory;
 
 @Configuration
+@Slf4j
 public class HandbookConfig {
     @Bean
     public static ExecutorService executorService() {
@@ -20,5 +30,27 @@ public class HandbookConfig {
 
         return Executors.newFixedThreadPool(
                 Runtime.getRuntime().availableProcessors(), threadFactory);
+    }
+
+    @Bean
+    public Jdbi jdbi(DataSource dataSource, List<JdbiPlugin> jdbiPlugins, List<RowMapper<?>> rowMappers) {
+        TransactionAwareDataSourceProxy proxy = new TransactionAwareDataSourceProxy(dataSource);
+        Jdbi jdbi = Jdbi.create(proxy);
+        log.info("Installing plugins... ({} found)", jdbiPlugins.size());
+        jdbiPlugins.forEach(jdbi::installPlugin);
+        log.info("Installing rowMappers... ({} found)", rowMappers.size());
+        rowMappers.forEach(jdbi::registerRowMapper);
+
+        return jdbi;
+    }
+
+    @Bean
+    public JdbiPlugin sqlObjectPlugin() {
+        return new SqlObjectPlugin();
+    }
+
+    @Bean
+    public JdbiPlugin h2DatabasePlugin() {
+        return new H2DatabasePlugin();
     }
 }
